406b42a89b5b156181793ff1d00d6d20233f98a5
SongJF <18080631973@163.com>
2018-08-07 11:08:15 +0800

    主窗口及关闭功能

--- ENBpresetAssistant/MainWindow.xaml.cs
+++ ENBpresetAssistant/MainWindow.xaml.cs
@@ -13,16 +13,30 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using MaterialDesignThemes.Wpf;
+
 namespace ENBpresetAssistant
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow :Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 窗口关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void DialogHost_DialogClosing(object sender, DialogClosingEventArgs eventArgs)
+        {
+            if (!Equals(eventArgs.Parameter, true)) return;
+
+            Application.Current.Shutdown();
         }
     }
 }
